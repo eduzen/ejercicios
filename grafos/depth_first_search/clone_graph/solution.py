e7010e9c5208b11a39a752404b2b64f5fb4ba2c8
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-06-16 16:42:49 +0200

    wip

--- grafos/depth_first_search/clone_graph/solution.py
+++ grafos/depth_first_search/clone_graph/solution.py
@@ -1,47 +1,22 @@
-from collections import deque
-
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-    def __eq__(self, node: object) -> bool:
-        return self.val == node.val and self.neighbors == node.neighbors
 
-    def __repr__(self) -> str:
-        return f'Node(val={self.val}, neighbors={self.neighbors})'
+def clone_node(node: Node) -> Node:
+    old_to_new = {}
 
+    def dfs_clone(node: Node) -> Node:
+        if node in old_to_new:
+            return old_to_new[node]
 
-class Graph:
-    def __init__(self):
-        self.visited = {}
-
-    def cloneGraph(self, neighbors: list[list[int, int]]) -> 'Node':
-        if node is None:
-            return node
-
-        return self.dfs(node)
-
-    def dfs(self, node: 'Node') -> 'Node':
-        if node in self.visited:
-            return self.visited[node]
-
-        clone = Node(node.val, [])
-        self.visited[node] = clone
-
+        copy = Node(node.val)
+        old_to_new[node] = copy
         for neighbor in node.neighbors:
-            clone.neighbors.append(self.dfs(neighbor))
+            copy.neighbors.append(dfs_clone(neighbor))
+        return copy
 
-        return clone
+    return dfs_clone(node) if node else None
 
 
-def main() -> None:
-    adjList = [[2,4],[1,3],[2,4],[1,3]]
-    graph = Graph()
-    cloned_graph = graph.cloneGraph(adjList)
-
-    assert cloned_graph == [[2,4],[1,3],[2,4],[1,3]], f'output={cloned_graph}'
-    print('Accepted')
-
-if __name__ == '__main__':
-    main()
